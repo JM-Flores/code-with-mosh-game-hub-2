ce9db446cbee11df0acbc4ff1e3304c5eca26666
JM-Flores <jmiguelflores19@gmail.com>
2024-08-22 16:33:23 +0800

    Refactor all fetches with react query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,21 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "../services/api-client";
 
-interface Platform {
+export interface Platform {
     id: number;
     name: string;
     slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+const usePlatforms = () => {
+    return useQuery({
+        queryKey: ['platforms'],
+        queryFn: () => apiClient.get<FetchResponse<Platform>>('/platforms').then(res => res.data),
+        staleTime: 24 * 60 * 60 * 1000, //24 hours
+        initialData: {count: platforms.length, results: platforms}
+    })
+};
 
 export default usePlatforms;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,27 @@
 import axios from "axios";
 
-export default axios.create({
+export interface FetchResponse<T> {
+    count: number;
+    results: T[];
+  }
+
+const axiosInstance = axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
         key: 'f12183e770db41d5bb8ac0ff1eb85513'
     }
 })
+
+class APIClient<T> {
+    endpoint: string;
+
+    constructor(endpoint: string){
+        this.endpoint=endpoint;
+    }
+
+    get() {
+        () => axiosInstance.get<FetchResponse<T>>(this.endpoint).then(res => res.data)
+    }
+}
+
+export default axiosInstance;
