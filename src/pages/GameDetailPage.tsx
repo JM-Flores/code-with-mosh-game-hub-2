7c911ee7717985f5607dc9ae997d3dea40860986
JM-Flores <jmiguelflores19@gmail.com>
2024-08-26 22:08:19 +0800

    Build expandable text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,11 +1,16 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
 
   const { data: game, isLoading, error } = useGame(slug!);
+
+  const [isTruncated, setIsTruncated] = useState(true);
+  const maxLength = 200;
 
   if (isLoading) return <Spinner />;
 
@@ -14,7 +19,7 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
