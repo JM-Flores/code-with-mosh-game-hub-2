918ea12ab5398dc38a13cf7237925e3b97279887
JM-Flores <jmiguelflores19@gmail.com>
2024-08-26 22:15:15 +0800

    Refactor: remove unused lines

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,16 +1,12 @@
+import { Grid, Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
-import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
-import { useState } from "react";
 import ExpandableText from "../components/ExpandableText";
+import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
 
   const { data: game, isLoading, error } = useGame(slug!);
-
-  const [isTruncated, setIsTruncated] = useState(true);
-  const maxLength = 200;
 
   if (isLoading) return <Spinner />;
 
@@ -20,6 +16,7 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <Grid></Grid>
     </>
   );
 };
